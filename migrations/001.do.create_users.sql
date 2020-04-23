d21531fc27b4cecffdbc1bd4c38bb0a62d9399b8
Sterling Watts <SterlingDWatts@gmail.com>
2020-04-23 15:26:51 -0700

    First commit

---
+++ migrations/001.do.create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  nickname TEXT,
+  date_created TIMESTAMP DEFAULT now() NOT NULL,
+);
---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  API_TOKEN: process.env.API_TOKEN || "42545a74-16e3-4982-b0d6-aab2d7ad68ee",
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://pleaseUpdateMe",
+  JWT_SECRET: process.env.JWT_SECRET || "590066f6-0db6-4227-8539-d334adad9e65",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "7h"
+};
